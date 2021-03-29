3e94176eafb2361f5b836336f3e5c245e9ca1e30
Administrator <oxf5deb3@163.com>
2021-03-30 01:04:38 +0800

    框架

---
+++ src/main/java/com/qlsofti/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.qlsofti.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author ${user}
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+}
