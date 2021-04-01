8ed245a28c6ebd7e948d5828a9b751b3edecb99a
Administrator <oxf5deb3@163.com>
2021-04-01 13:18:54 +0800

    项目初始化

--- noise-app/src/main/java/com/qlsofti/controller/UserController.java
+++ noise-app/src/main/java/com/qlsofti/controller/UserController.java
@@ -2,18 +2,15 @@ package com.qlsofti.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author ${user}
  */
 @Controller
-@RequestMapping("/user")
 public class UserController {
 
     @RequestMapping("/finduri")
-    @ResponseBody
     public String getUrl(){
-        return "hellow world.";
+        return "index";
     }
 }
